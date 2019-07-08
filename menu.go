c7e7a8a8a1e556f7862d9f1fffecc5b5228bf8b9
unknown <greensBurst@163.com>
2019-07-08 10:58:42 +0800

    登记收入

--- menu.go
+++ menu.go
@@ -6,10 +6,20 @@ func main() {
 	//接受用户输入变量
 	var key string
 
+	//账户金额
+	balance := 10000.0
+	//每次收支金额
+	money := 0.0
+	//每次收支说明
+	note := ""
+	//收支详情，使用字符串记录
+	detail := "收    支\t账户金额\t收支金额\t说    明"
+
+
 	//显示这个主菜单
 	loop:
 	for {
-		fmt.Println("----------家庭收支记账软件----------")
+		fmt.Println("\n----------家庭收支记账软件----------")
 		fmt.Println("            1 收支明细")
 		fmt.Println("            2 登记收入")
 		fmt.Println("            3 登记支出")
@@ -19,8 +29,17 @@ func main() {
 		fmt.Scanln(&key)
 
 		switch key {
-		case "1":fmt.Println("----------当前收支明细记录----------")
+		case "1":
+			fmt.Println("----------当前收支明细记录----------")
+			fmt.Println(detail)
 		case "2":
+			fmt.Print("本次收入金额:")
+			fmt.Scanln(&money)
+			balance += money	//修改账户余额
+			fmt.Print("本次收入说明:")
+			fmt.Scanln(&note)
+			//将这个收入情况，拼接到detail变量
+			detail += fmt.Sprintf("\n收    入\t%v\t\t%v\t\t%v",balance,money,note)
 		case "3":fmt.Println("登记支出..")
 		case "4":break loop
 		default:fmt.Println("请输入正确的选项..")
